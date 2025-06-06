775c6c49a97c42992fa7c40410f4b1134fd9eb72
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2025-06-06 15:52:04 -0400

    comment out line saving csv file. Add chunk recreating get_species_dist.R from ecodata

--- R/species_dist.R
+++ R/species_dist.R
@@ -205,10 +205,53 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       # excluding 2020-2023
       dplyr::filter(!YEAR %in% c(2020,2023))
     
-    #write output
-    write.csv(x = dist_mean, file = here::here("data/species_dist_spring.csv"), row.names = FALSE)
+    # #write output
+    # write.csv(x = dist_mean, file = here::here("data/species_dist_spring.csv"), row.names = FALSE)
+    
+    # no longer writing csv, just going straight to species_dist function
     
   } else {
     stop("One or more of the input files are not present in the location specified")
   }
 }
+
+# From ecodata/data-raw/get_species_dist.R -----------------------------------------
+# https://github.com/NOAA-EDAB/ecodata/blob/master/data-raw/get_species_dist.R
+
+get_species_dist <- function(save_clean = F){
+  
+  species_dist <- dist_mean |> 
+    dplyr::rename(`along-shelf distance` = alongshelf,
+                  `distance to coast` = coast,
+                  Time = YEAR) |> 
+    tidyr::pivot_longer(-Time, names_to = "Var", values_to = "Value") |> 
+    dplyr::mutate(EPU = "All",
+                  Units = ifelse(stringr::str_detect(Var,"distance"),"km",
+                                 ifelse(stringr::str_detect(Var,"Latitude"),
+                                        "degreesN",ifelse(stringr::str_detect(Var,"Longitude"),
+                                                          "degreesW",ifelse(stringr::str_detect(Var, "depth"),
+                                                                            "m",NA)))))
+  
+  # Fill in missing data with NAs
+  expanded <- expand.grid(Time = min(species_dist$Time):max(species_dist$Time),
+                          Var = unique(species_dist$Var))
+  
+  species_dist <- dplyr::right_join(species_dist, expanded) |> 
+    dplyr::arrange(Time)
+  
+  # metadata ----
+  attr(species_dist, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/species-distribution-indicators.html"
+  # attr(species_dist, "data_files")   <- list(
+  #   species_dist_csv = species_dist_csv)
+  attr(species_dist, "data_steward") <- c(
+    "Kevin Friedland <kevin.freidland@noaa.gov>")
+  attr(species_dist, "plot_script") <- list(
+    `mf_MAB` = "macrofauna_MAB.Rmd-species-dist.R")
+  
+  if (save_clean){
+    usethis::use_data(species_dist, overwrite = T)
+  } else {
+    return(species_dist)
+  }
+}
+get_species_dist(save_clean = T)
