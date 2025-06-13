b11ccfaf6875d7ea76b9bf7bf4fd9bde6b88d381
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2025-06-13 15:18:02 -0400

    cleaned up script after comparing Kevin and Sarah's methods

--- R/species_dist.R
+++ R/species_dist.R
@@ -21,18 +21,20 @@
 #' @export
 
 
-
+# input data for testing
+inputPathSurvey <- here::here("data-raw/surveyNoLengths.rds")
+inputPathSpecies <- here::here("data-raw/SOE_species_list_24.rds")
 
 create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = NULL) {
  
   end.year <- format(Sys.Date(),"%Y")
    
-  # Check if the input files exist ---------------------------
+# Check if the input files exist ---------------------------
   if (file.exists(inputPathSurvey) && file.exists(inputPathSpecies)) {
     
     
 
-    # read in data from RDS files -------------------------------------------
+# read survey data -------------------------------------------
     # and check to make sure data is in the right format
     survey <- readRDS(inputPathSurvey)
     if(!is.data.frame(survey$survdat)) {
@@ -43,7 +45,7 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       dplyr::filter(YEAR <= end.year) |>
       data.table::as.data.table()
     
-    #Grab species list
+# read species list -------------
     species <- readRDS(inputPathSpecies) |>
       dplyr::as_tibble() |>
       data.table::as.data.table()
@@ -52,55 +54,37 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       dplyr::select(SVSPP, SOE.24, Fed.Managed) |>
       dplyr::distinct()
 
-    
-    
-    
- # edited from Sarah W.'s script  ------------
-    # https://github.com/NEFSC/READ-EDAB-CurrentConditions/blob/main/R/species_dist_prep_from_kevin.R
-    
-    ## set numsps -----------------------------
+# set numsps -----------------------------
 
     numsps=nrow(species)
     
-    ## select the 48 best model distribution species --------------------
-       # not sure where these came from
+# select the 48 best model distribution species --------------------
     
     spptokeep <- c(13,15,22,23,24,25,26,27,28,32,33,34,35,72,73,74,75,76,77,78,84,102,103,104,105,106,107,108,109,121,131,141,143,155,156,163,164,171,172,176,181,192,193,197,301,401,502,503)
     
-    ## read in depth grid ------------------------------------------------
+# raster depth grid ------------------------------------------------
     gdepth <- raster::raster("data-raw/nes_bath_data.nc",band=1)
     
-    ## read in coordinates for along shelf diagnal  diag.csv -----------------
+# read coordinates for along shelf diagonal  diag.csv -----------------
     diag <- read.csv(here::here("data-raw/diag.csv"))
     
-    ## read in coordinate for coast     nes_coastline.csv -----------------
-    # nescoast <- read.csv(here::here("data-raw/nes_coastline.csv"))
-    
-    # not sure why this changed but nescoast is never used again
-    
-    ## read in coordinate for coast     nes_coast_2.csv --------------------
+# read coordinate for coast --------------------
     nescoast2 <- read.csv(here::here("data-raw/nes_coast_2.csv"))
     
-    # # read in coordinate for coast     hersey_high_2.csv
-    # nesc2 <- read.csv(here::here("data-raw/hersey_high_2.csv"))
-    
-    # this was never used again so commenting out for now
-    
-    
-    ## constants ------------------------------------------------------------
+# constants ------------------------------------------------------------
     radt=pi/180
     R <- 6371 # Earth mean radius [km]
     
-    ## load stratareas ---------------------------------------------------
+# load stratareas ---------------------------------------------------
     load(here::here("data-raw/stratareas.rdata"))
     
-    ## tidy data ---------------------------------------------------
+# tidy data ---------------------------------------------------
     # trim the data, filter for chosen season
     survdat_spring <- survdat  |> 
       dplyr::select("CRUISE6","STATION","STRATUM","SVSPP","YEAR","SEASON","LAT","LON","DEPTH","ABUNDANCE","BIOMASS")  |> 
       dplyr::filter(SEASON == "SPRING")
     
-    ## strata to use -----------------------------------------------------
+# strata to use -----------------------------------------------------
     # offshore strata to use
     CoreOffshoreStrata <- c(seq(1010,1300,10),1340, seq(1360,1400,10),seq(1610,1760,10))
     # inshore strata to use, still sampled by Bigelow
@@ -108,45 +92,46 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
     # combine
     strata_used <- c(CoreOffshoreStrata,CoreInshore73to12)
     
-    ## summarize biomass --------------------------------------------------
+# summarize biomass --------------------------------------------------
     survdat_spring <- survdat_spring  |> 
       dplyr::filter(STRATUM %in% strata_used)  |>  #chosen strata only
       dplyr::filter(SVSPP %in% spptokeep)  |>  #chosen species only
       dplyr::distinct()  |>  #gets rid of length and weight info, retains unique station info
       dplyr::mutate(LOGBIO = round(log10(BIOMASS * 10+10))) #rounded biomass scalar
     
-    ## summarize stations ---------------------------------
+# summarize stations ---------------------------------
     survdat_stations <- survdat_spring |> 
       dplyr::select("CRUISE6","STATION","STRATUM","YEAR") |> 
       dplyr::distinct()
     
-    ## count how many times each stratum was sampled in each year --------------
+# count how many times each stratum was sampled in each year --------------
     strata_tally <- survdat_stations  |> 
       dplyr::group_by(STRATUM,YEAR) |> 
       dplyr::tally()
     
-    ## filter for chosen strata only -------------------
+# filter for chosen strata only -------------------
     stratareas <- stratareas  |> 
       dplyr::filter(STRATA %in% strata_used) |> 
       dplyr::rename(STRATUM = STRATA)
     
-    ## merge tally with area to be able to divide -------------------------
+# merge tally with area to be able to divide -------------------------
     strata <- dplyr::left_join(strata_tally,stratareas,by="STRATUM")  |> 
       dplyr::mutate(eff_area = AREA/n)
     
-    ## join survey data with effective area ------------------------------
+# join survey data with effective area ------------------------------
     survdat_spring <- dplyr::left_join(survdat_spring,strata, by=c("YEAR","STRATUM")) 
     
     survdat_spring <- survdat_spring |> 
       dplyr::mutate(PLOTWT = eff_area*LOGBIO) #really not sure why
     
     ## Plot stations ---------------------------------------------------
-    plot(survdat$LON[survdat_spring$YEAR==1974],survdat$LAT[survdat_spring$YEAR==1974])
+    # visual test
+    # plot(survdat$LON[survdat_spring$YEAR==1974],survdat$LAT[survdat_spring$YEAR==1974])
     
-    ## number of records to evaluate -----------------------------------
+# number of records to evaluate -----------------------------------
     numrecs=nrow(survdat_spring)
     
- ## distance to coast using geosphere -----------------------------------
+# distance to coast using geosphere -----------------------------------
     
     ####  Geosphere package to calc distance to coastline from pts (lon,lat), returns meters
     dd = array(data = NA, dim = nrow(survdat_spring))
@@ -156,7 +141,7 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
     dd <- as.data.frame(geosphere::dist2Line(pts[,], line_nescoast2))
     survdat_spring$GDTOC <- dd$distance/1000 # convert meters to KM
     
-## diag distance using geosphere -------------------------------------------
+# diag distance using geosphere -------------------------------------------
     
     # Find distance to diagonal line (diag), use coordinates of nearest point to find distance to NC outerbanks (min(diag))
     dd2 = array(data = NA, dim = nrow(survdat_spring))
@@ -169,7 +154,7 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
     survdat_spring$GASDIST = distNC
     
     
-## fill in missing depths -------------------------------------------
+# fill in missing depths -------------------------------------------
     
     # find cases with missing depth data
     missingdepth=which(is.na(survdat_spring$DEPTH))
@@ -179,7 +164,7 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       survdat_spring$DEPTH[k] = raster::extract(gdepth,cbind(survdat_spring$LON[k],survdat_spring$LAT[k])) * -1
     }
     
-## create spring_summary ---------------------------------------------------------
+# create spring_summary ---------------------------------------------------------
     
     spring_summary <- survdat_spring  |> 
       dplyr::group_by(SVSPP,YEAR) |> 
@@ -191,10 +176,7 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       dplyr::select(YEAR,SVSPP,swDEPTH,swLAT,swLON,swGASDIST,swGDTOC) |>
       dplyr::distinct()
 
-# edited from another one of Sarah W.'s scripts ------------------------------------------------
-# https://github.com/NEFSC/READ-EDAB-CurrentConditions/blob/main/R/species_dist_from_kevin.R
-    
-## calculate means for three metrics of interest ------------------------------------------------
+# calculate means for three metrics of interest ------------------------------------------------
     
     #calculate means for three metrics of interest
     #ignoring 2020 because of survey disruptions
@@ -203,21 +185,18 @@ create_species_dist <- function(inputPathSurvey, inputPathSpecies, outputPath = 
       dplyr::summarise(alongshelf = mean(swGASDIST, na.rm = T), 
                        depth = mean(swDEPTH, na.rm = T),
                        coast = mean(swGDTOC, na.rm = T)) |> 
-      # excluding 2020-2023
+      # excluding 2020 & 2023
       dplyr::filter(!YEAR %in% c(2020,2023))
     
     # #write output
     # write.csv(x = dist_mean, file = here::here("data/species_dist_spring.csv"), row.names = FALSE)
     
-    # no longer writing csv, just going straight to species_dist function
     
   } else {
     stop("One or more of the input files are not present in the location specified")
   }
 }
 
-# From ecodata/data-raw/get_species_dist.R -----------------------------------------
-# https://github.com/NOAA-EDAB/ecodata/blob/master/data-raw/get_species_dist.R
 
 get_species_dist <- function(save_clean = F){
   
