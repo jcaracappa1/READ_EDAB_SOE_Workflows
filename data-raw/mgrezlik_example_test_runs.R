33a44af06b6364207a26a5fff1c81c5a870cc77a
Max Grezlik <maxwell.grezlik@noaa.gov>
2025-06-24 09:49:23 -0500

    added staticPath for test runs

--- data-raw/mgrezlik_example_test_runs.R
+++ data-raw/mgrezlik_example_test_runs.R
@@ -1,10 +1,11 @@
 # Example of how to test on the Rstudio container 
-outputPathDataSets <- "~/EDAB_Dev/beet"
-outputPath <- "~/EDAB_Dev/beet"
-inputPathSurvey <- "~/EDAB_Dev/beet/surveyNoLengths.rds"
-inputPathSpecies <- "~/EDAB_Datasets/SOE_species_list_24.rds"
-inputPathAlbatross <- "~/EDAB_Dev/beet/albatrossData.rds"
-inputPathBigelow <- "~/EDAB_Dev/beet/bigelowData.rds"
+outputPathDataSets <- "/home/mgrezlik/EDAB_Dev/beet"
+outputPath <- "/home/mgrezlik/EDAB_Dev/beet"
+inputPathSurvey <- "/home/mgrezlik/EDAB_Dev/beet/surveyNoLengths.rds"
+inputPathSpecies <- "/home/mgrezlik/EDAB_Datasets/SOE_species_list_24.rds"
+inputPathAlbatross <- "/home/mgrezlik/EDAB_Dev/beet/albatrossData.rds"
+inputPathBigelow <- "/home/mgrezlik/EDAB_Dev/beet/bigelowData.rds"
+staticPath <-  "/home/mgrezlik/EDAB_Resources/"
 
 source(here::here("data-raw/workflow_species_dist.R"))
 
@@ -12,7 +13,7 @@ source(here::here("data-raw/workflow_species_dist.R"))
 
 # rawData <- SOEworkflows::get_survey_data(channel,outputPathDataSets)
 
-indD <- workflow_species_distribution(outputPath = outputPath,
-                                   inputPathSurvey = inputPathSurvey,
-                                   inputPathSpecies = inputPathSpecies)
+# indD <- workflow_species_distribution(outputPath = outputPath,
+#                                    inputPathSurvey = inputPathSurvey,
+#                                    inputPathSpecies = inputPathSpecies)
 
