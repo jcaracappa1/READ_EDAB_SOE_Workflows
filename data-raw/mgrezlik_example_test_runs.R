0ba0deaf469ecc34f4bb9c7a15848b7c2cf6fe50
Max Grezlik <maxwell.grezlik@noaa.gov>
2025-07-11 13:24:03 -0500

    testing comdat and creating comparison tables of pipeline values and ecodata::comdat values

--- data-raw/mgrezlik_example_test_runs.R
+++ data-raw/mgrezlik_example_test_runs.R
@@ -22,3 +22,48 @@ source(here::here("data-raw/workflow_species_dist.R"))
 #                                    inputPathSurvey = inputPathSurvey,
 #                                    inputPathSpecies = inputPathSpecies)
 
+
+# testing comdat ---------------------
+
+commercial_summary <- create_comdat(
+  comdat_path <- '/home/mgrezlik/EDAB_Dev/beet/commercial_comdat.rds',
+  report_year = 2025,
+  end_year = 2024,
+  input_path_species <- "/home/mgrezlik/EDAB_Datasets/SOE_species_list_24.rds",
+  menhaden_path <- "/home/mgrezlik/EDAB_Datasets/AM Landings by Gaichas Regions 1967-2024.xlsx"
+)
+
+comdat <- get_comdat(
+  processed_comdat = commercial_summary, 
+  save_for_package = FALSE # Set to FALSE to see the result directly
+)
+
+## comparing my comdat outputs to old comdat ---------------
+comdat_max <- comdat |> 
+                dplyr::mutate(source = 'max')
+
+comdat_ecodata <- ecodata::comdat |> 
+                    dplyr::mutate(source = 'ecodata')
+
+comdat_compare <- dplyr::bind_rows(comdat_max, comdat_ecodata)
+
+difference_summary <- comdat_compare |>
+  tidyr::pivot_wider(
+    names_from = source,
+    values_from = Value
+  ) |>
+  # Calculate both absolute and percentage difference
+  dplyr::mutate(
+    absolute_diff = max - ecodata,
+    percent_diff = (max - ecodata) / ecodata * 100
+  )
+
+
+# Pipeline method has more observations than are found in ecodata (34037 compared to 26141)
+# anti join to find observations in comdat_max that are not in comdat_ecodata
+
+missing_from_ecodata <- dplyr::anti_join(
+  comdat_max,
+  comdat_ecodata,
+  by = c("Time", "Var", "EPU")
+)
