51dc83474dc3594ae4c464bb907a80cb0d98784e
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-07-23 14:21:48 -0400

    feat(trans_dates): add scripts for the calculation, workflow and test run for the transition dates indicator

---
+++ data-raw/workflow_trans_dates.R
@@ -0,0 +1,43 @@
+#' Calculates trans_dates data set for automated workflow
+#'
+#' This uses a static input file from Kevin Friedland.
+#' It is formatted exactly like the ecodata data object
+#'
+#' @param inputPath Character string. Full path to the SST input file from Kevin Friedland
+#' @param outputPath Character string. Path to folder where data pull should be saved
+#'
+#' @example
+#' \dontrun{
+#' # create the ecodata::trans_dates indicator
+#' workflow_trans_dates(inputPath = "path/to/input/data.csv",
+#'                      outputPath = "path/to/output/folder")
+#'
+#' }
+#'
+#'
+#' @return ecodata::trans_dates data frame
+#'
+#' @section Dependencies:
+#' 
+#' This assumes that the input data file from Kevin Friedland has been provided and resides in the path `inputPath`
+#'
+#' @export
+
+
+
+workflow_trans_dates <- function(inputPath, outputPath = NULL) {
+  
+  # Assumes that input data has been provided
+  
+  # Add check to skip running workflow if data not present
+  if(file.exists(inputPath) && (!is.null(outputPath))) {
+    
+    indicatorData <- SOEworkflows::create_trans_dates(inputPath = inputPath)
+
+    # write data to file
+    saveRDS(indicatorData,paste0(outputPath,"/trans_dates.rds"))
+  } else {
+    #
+    message("The input file is not present in the location specified")
+  }
+}
